e0659d174ffa314570a10876beff2e10cd3055bd
Anel Gabriel Dominguez <anelgabriel002@gmail.com>
2021-03-24 13:18:52 -0400

    Create Footer.js

---
+++ galeria-app/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+
+class Footer extends Component {
+
+
+    render(){
+        return (
+            <footer className="pie-pagina">
+                Made by Anel Dominguez &copy;
+            </footer>
+        );
+    }
+}
+
+
+export default Footer;
